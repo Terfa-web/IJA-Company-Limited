d34dd8198f0477cb207adfe06c128e18aae3f6f0
Ter-web <n.terfa@yahoo.com>
2024-01-09 15:56:35 +0200

    Build the footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,99 @@
 import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+
+const quickLinks = [
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/#",
+    display: "Privacy Policy",
+  },
+  {
+    path: "/cars",
+    display: "Car Listing",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo">
+              <h1>
+                <Link to="/home" className=" d-flex align-items-center gap-3">
+                  <i class="ri-car-line"></i>
+                  <span>
+                    Rent car <br /> Service
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laudantium maiores eligendi accusamus ipsam non similique mollitia
+              natus! Obcaecati, architecto voluptate!
+            </p>
+          </Col>
+
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Head Office</h5>
+              <p className="office__info">
+                123 Imaginary Street, Tech District, Victoria Island, Lagos,
+                Nigeria.
+              </p>
+              <p className="office__info">Phone: +234-456-342-90</p>
+              <p className="office__info">Email: imagine@hotmail.co.ng</p>
+              <p className="office__info">Office Time: 10am - 7pm </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">
+                {" "}
+                Subscribe to our newsletter
+              </p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
