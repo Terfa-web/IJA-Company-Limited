b8472f191eba4f468be1c5f0c339134e77aa0d33
Ter-web <n.terfa@yahoo.com>
2024-01-09 21:10:25 +0200

    Build the Slider component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,12 +27,14 @@ const quickLinks = [
 ];
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <footer className="footer">
       <Container>
         <Row>
           <Col lg="4" md="4" sm="12">
-            <div className="logo">
+            <div className="logo footer__logo">
               <h1>
                 <Link to="/home" className=" d-flex align-items-center gap-3">
                   <i class="ri-car-line"></i>
@@ -50,11 +52,11 @@ const Footer = () => {
           </Col>
 
           <Col lg="2" md="4" sm="6">
-            <div className="mb-4">
+            <div className="mb-4 ">
               <h5 className="footer__link-title">Quick Links</h5>
               <ListGroup>
                 {quickLinks.map((item, index) => (
-                  <ListGroupItem key={index} className="p-0 mt-3">
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__links ">
                     <Link to={item.path}>{item.display}</Link>
                   </ListGroupItem>
                 ))}
@@ -64,7 +66,7 @@ const Footer = () => {
 
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
-              <h5 className="footer__link-title">Head Office</h5>
+              <h5 className="footer__link-title mb-4">Head Office</h5>
               <p className="office__info">
                 123 Imaginary Street, Tech District, Victoria Island, Lagos,
                 Nigeria.
@@ -77,7 +79,7 @@ const Footer = () => {
 
           <Col lg="3" md="4">
             <div className="mb-4">
-              <h5 className="footer__link-title">Newsletter</h5>
+              <h5 className="footer__link-title mb-4">Newsletter</h5>
               <p className="section__description">
                 {" "}
                 Subscribe to our newsletter
@@ -88,6 +90,15 @@ const Footer = () => {
                   <i class="ri-send-plane-line"></i>
                 </span>
               </div>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>Copyright {year}, Developed by
+                Terfa-web. All rights reserved.
+              </p>
             </div>
           </Col>
         </Row>
