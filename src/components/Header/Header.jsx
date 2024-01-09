26be4b53a21e50685ba09b38fb38bc530c9cf0ec
Ter-web <n.terfa@yahoo.com>
2024-01-09 11:12:11 +0200

    build Header middle content structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import "../../styles/header.css";
 const Header = () => {
   return (
     <header className="header">
+      {/* =======header-top======= */}
       <div className="header-top">
         <Container>
           <Row>
@@ -17,14 +19,65 @@ const Header = () => {
             </Col>
 
             <Col lg="6" md="6" sm="6">
-              <div className="header__top__right">
-                <Link to="#">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i class="ri-login-circle-line"></i> Login
                 </Link>
-                <Link to="#">
+                <Link to="#" className="d-flex align-items-center gap-1">
                   <i class="ri-user-line"></i>Register
                 </Link>
               </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* ======== header middle ====== */}
+      <div className="header__middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home">
+                    <i class="ri-car-line"></i>
+                    <span>
+                      Rent car <br /> Service
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Nigeria</h4>
+                  <h6>Lagos City, Nigeria</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location">
+                <span>
+                  <i class="ri-time-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Sunday to Friday</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="2" md="3" sm="0">
+              <button className="heder__btn btn">
+                <Link to="/contact">
+                  <i class="ri-pnone-line"></i> Request a call
+                </Link>
+              </button>
             </Col>
           </Row>
         </Container>
