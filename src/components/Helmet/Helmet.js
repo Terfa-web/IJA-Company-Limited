c29b463d99457f973a5b1264d98e22fc32a402f5
Ter-web <n.terfa@yahoo.com>
2024-06-29 11:51:14 +0100

    delette findcar form

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Helmet = (props) => {
 
-  document.title = 'Rent Car Service -' + props.title;
+  document.title = 'Solar Panel and CCTV Services -' + props.title;
   return (
     <div className='w-100'>{props.children}</div>
   )
