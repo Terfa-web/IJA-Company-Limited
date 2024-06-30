b724a75b36960b588bb9f544f3b8ceebff351dcf
Ter-web <n.terfa@yahoo.com>
2024-06-30 17:59:33 +0100

    update the text content under our services section

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -1,44 +1,44 @@
 const serviceData = [
   {
     id: 1,
-    title: "City Transfer",
+    title: "Installation and Setup",
     icon: "ri-map-pin-2-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Professional installation of solar panels, inverters, and mounting systems. Ensure proper wiring, configuration, and integration with existing electrical systems.",
   },
 
   {
     id: 2,
-    title: "Whole  City Tour",
+    title: "Maintenance and Repairs",
     icon: "ri-community-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Offer regular maintenance services to ensure optimal performance. Provide troubleshooting and repair services for any issues that may arise.",
   },
 
   {
     id: 3,
-    title: "Unlimited Miles Car Rental",
+    title: "Energy Storage Solutions",
     icon: "ri-roadster-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Offer battery storage options to store excess solar energy. Provide appliances to enable integrated solar energy storage systems.",
   },
 
   {
     id: 4,
-    title: "Fast & Easy Booking",
+    title: "Site Survey and Security Assessment",
     icon: "ri-timer-flash-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Conduct thorough site surveys to identify security vulnerabilities. Providing expert recommendations on camera placement and system design.",
   },
 
   {
     id: 5,
-    title: "Many Pickup Locations",
+    title: "Installation and Configuration",
     icon: "ri-map-pin-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Professional installation of CCTV cameras, DVRs/NVRs, and related equipments. Ensuring proper configuration and integration with existing security systems.",
   },
 
   {
     id: 6,
-    title: "Airport Transfer",
+    title: "Remote Monitoring and Access",
     icon: "ri-flight-takeoff-line",
-    desc: "Tempor tempor sadipscing vero lorem sea, invidunt sed et eos ipsum et erat. Dolor ut duo sadipscing lorem. Gubergren gub",
+    desc: "Set up remote monitoring capabilities for real-time access via smartphones or computers. Providing training on how to use remote access and monitoring tools.",
   },
 ];
 
